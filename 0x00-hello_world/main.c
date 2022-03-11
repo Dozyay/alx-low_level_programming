73f5b93e11c8bea2d026b2cee8e067cd4360c66d
Amos-Ejesi Nnaedozie <amosejesi.dozie@gmail.com>
2022-03-11 08:23:13 -0800

    dozyay's Ccommit 1

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,8 +1,8 @@
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	    return (0);
