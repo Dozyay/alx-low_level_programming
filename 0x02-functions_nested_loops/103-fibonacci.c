57bc27e30357ef50b339a0aae81e72c07d6bc6c4
Amos-Ejesi Nnaedozie <amosejesi.dozie@gmail.com>
2022-03-17 06:09:43 -0700

    dozy's commit 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci sequence
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long sum = 0;
+
+	long t1 = 1;
+
+	long t2 = 2;
+
+	long nextTerm = t1 + t2;
+
+	if (t1 % 2 == 0)
+		sum += t1;
+	else if (t2 % 2 == 0)
+		sum += t2;
+	while (nextTerm <= 4000000)
+	{
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+		if (nextTerm % 2 == 0)
+			sum += nextTerm;
+	}
+
+		printf("%ld\n", sum);
+		return (0);
+}
