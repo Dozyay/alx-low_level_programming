52144768e5e4fb1a5c30bc1421ebf53e1150eb6a
Amos-Ejesi Nnaedozie <amosejesi.dozie@gmail.com>
2022-03-17 06:02:50 -0700

    dozy's commit 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,26 +2,33 @@
 
 /**
  * main - first 50 fibonacci
+ * starting with 1 and 2, followed by a new line.
  *
  * Return: void
  */
 int main(void)
 {
-	long int a, b, s, i;
+	int i;
 
-	a = 1;
-	b = 2;
-	s = 3;
-	printf("%ld, ", a);
-	printf("%ld, ", a);
-	for (i = 3; i < 50; i++)
+	long t1 = 1;
+
+	long t2 = 2;
+
+	long nextTerm = t1 + t2;
+
+	printf("%ld, %ld, ", t1, t2);
+	for (i = 3; i < 51; i++)
 	{
-		printf("%ld, ", s);
-		a = b;
-		b = s;
-		s = a + b;
+		if (i == 50)
+			printf("%ld", nextTerm);
+		else
+		{
+			printf("%ld, ", nextTerm);
+			t1 = t2;
+			t2 = nextTerm;
+			nextTerm = t1 + t2;
+		}
 	}
-	printf("%ld\n", s);
-
+	putchar('\n');
 	return (0);
 }
