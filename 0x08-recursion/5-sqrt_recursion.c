a71e01679403b3a721f855394ea1c51bd76b16ca
Amos-Ejesi Nnaedozie <amosejesi.dozie@gmail.com>
2022-03-30 15:49:52 -0700

    sqrt commit 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,32 @@
 #include "main.h"
-/**
- * sqtRecursive - computes square root recursively doing binary search
- * @start: inital number
- * @end: last number within the limit of number
- * @m: given number
- * Return: 1 if not found sqrroot, else sqrroot
- **/
-int sqtRecursive(int start, int end, int m)
-{
-	long mid;
 
-	if (end >= start)
-	{
-		mid = start + (end - start) / 2;
-		if (mid * mid == m)
-			return (mid);
-/* following binary search */
-		if (mid * mid > m)
-			return (sqtRecursive(start, mid - 1, m));
-		if (mid * mid < m)
-			return (sqtRecursive(mid + 1, end, m));
-	}
-	return (-1);
-}
 /**
- * _sqrt_recursion - finds the natural square root of a number
- * @n: given number
- * Return: square root of n or -1
- **/
-	int _sqrt_recursion(int n)
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val);
+
+int _sqrt_recursion(int n)
 {
-	if (n < 0)
+return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return  (square(n, val + 1));
+	else
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (sqtRecursive(2, n, n));
 }
-
-
- *
-
